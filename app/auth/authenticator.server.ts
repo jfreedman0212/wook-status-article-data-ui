6c39c9b6dc42c258f0351bbb63b1c72becbc012e
Josh Freedman <josh@freedman.dev>
2024-05-12 18:16:49 -0400

    Fix Auth0 integration

--- app/auth/authenticator.server.ts
+++ app/auth/authenticator.server.ts
@@ -2,7 +2,7 @@ import { Authenticator } from "remix-auth";
 import { cookieSessionStorage } from "~/auth/session.server";
 import {auth0Strategy} from "~/auth/auth0-strategy.server";
 
-const authenticator = new Authenticator(cookieSessionStorage);
+const authenticator = new Authenticator<string>(cookieSessionStorage);
 authenticator.use(auth0Strategy);
 
 export { authenticator };
