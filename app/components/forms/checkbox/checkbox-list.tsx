5905ccdd9ec75a12ca7232b4c8b0f897e800634f
Josh Freedman <josh@freedman.dev>
2024-05-18 14:19:45 -0400

    Minor fixes

--- app/components/forms/checkbox/checkbox-list.tsx
+++ app/components/forms/checkbox/checkbox-list.tsx
@@ -1,4 +1,4 @@
-import {forwardRef, ReactNode, useId} from "react";
+import {forwardRef, ReactNode, useId, useMemo} from "react";
 import {FormFieldProvider, useFormField} from "~/components/forms/form-field-context";
 import {CheckboxListContext} from "~/components/forms/checkbox/checkbox-list-context";
 import formFieldStyles from "../form-field.module.css";
@@ -19,11 +19,13 @@ type CheckboxListProps = {
 const CheckboxList = forwardRef<HTMLFieldSetElement, CheckboxListProps>(({ name, label, value, onChange, children, required, hint }, ref) => {
     const fieldId = `${useId()}__${name}`;
     
+    const checkboxContext = useMemo(() => ({ value, onChange }), [value, onChange])
+    
     return (
         <FormFieldProvider name={name} fieldId={fieldId} hint={hint} required={required}>
             <div className={formFieldStyles.formField}>
                 <Fieldset ref={ref} label={label}>
-                    <CheckboxListContext.Provider value={{ value, onChange }}>
+                    <CheckboxListContext.Provider value={checkboxContext}>
                         {children}
                     </CheckboxListContext.Provider>
                 </Fieldset>
